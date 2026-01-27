7ff6a08913fd755c864146028ea84def04235b52
Binshad K <binshadcs@gmail.com>
2026-01-27 14:36:26 +0530

    feat: vite pwa plugin added

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      '73be43b6de14.ngrok-free.app'
+    ]
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    // devOptions: {
+    //   enabled: true
+    // }
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
